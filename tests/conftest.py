9c6aa4f96949dad76642daa4538cc6cfa4873134
Xalios <mastep@yandex.ru>
2023-07-24 00:08:24 +0300

    new browser version

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,6 @@ from selene import Config, browser as br, Browser
 from qa_guru_hw10_tests.utils import attach
 
 RES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'resources'))
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -26,15 +18,12 @@ def load_env():
 def setup_browser(request):
     br.config.base_url = 'https://demoqa.com'
 
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    print(f'browser_version: {browser_version}')
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
